f9277d748d2f16fa7500755764e10184a1e8697e
circleone <1114025502@qq.com>
2019-08-07 10:44:43 +0800

    1

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,12 +3,14 @@ import './style.css';
 import OrderList from '../OrderList'
 import Header from '../Header'
 import 'antd-mobile/dist/antd-mobile.css';
+import LikeBtn from '../LikeBtn'
 // import Header from './components/Header'
 function App() {
   return (
     <div className="App">
       <Header />
       <OrderList />
+      <LikeBtn />
     </div>
   );
 }
